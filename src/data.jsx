a26c77dffb5afe9bbad6bbf378f3debcd931df57
chrisfejiro <fejiroayenor@gmail.com>
2024-06-06 09:48:19 +0100

    new

--- src/data.jsx
+++ src/data.jsx
@@ -99,7 +99,7 @@ export const personalInfo = [
     id: 7,
     title: "Address : ",
     description:
-      "Royal Estate Phase 2, Royal Estate Road, Games Village, off Eric Moore Road, off Bode Thomas, Surulere, Lagos",
+      "Surulere, Lagos",
   },
 
   {
